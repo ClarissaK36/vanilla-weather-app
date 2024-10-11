6423b25fcff663d7d638709030224291236acfec
ClarissaK36 <clarissak.official@gmail.com>
2024-10-11 21:54:35 +0200

    Updated weather search using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+
+}
+
+function searchCity(city){
+    let apiKey = "16dte5fe6bd880b3859a456a73aoea0f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+axios.get(apiUrl).then(refreshWeather);
+}
 
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+searchCity("Paris");
